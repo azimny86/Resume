f8f527312c8b4b39aa69cd25e87d9869d3f26e0e
azimny86 <azimny86@gmail.com>
2023-02-14 18:25:45 +0000

    Add fetch repo information for users

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,18 +1,37 @@
-function userInfoHTML(user){
-    return
-    `<h2>${user}</h2>
-        <span class="small-name>
-            (@<a hreh="${user.html_url} target= "_blank">${user.login}</a>
-        </sapn>
-    </h2>
-    <div class="gh-content">
-        <div class="gh-avatar">
-            <a href= "${user.html_url} target= "_blank">
-                <img src"${user.avatar_url}" width="80" height="80" alt="${user.login}"/>
-            </a>
-        </div>
-        <p>Fallowers: ${user.followers} - Fallowing ${user.following} <br> Repos: ${user.public} </p>
-    </div>`
+function userInformationHTML(user) {
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
+function repoInformationHTML(repos){
+    if(repos.length ==0) {
+        return `<div class="clearfix repo-list">No repos</div>`
+    }
+
+    var listItemHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`
+    });
+    return `<div class="clearfix repo-list>
+                <p> <strong>Repo List:</strong>
+                <ul>
+                    ${listItemHTML.join("\n")}
+                </ul>
+                </p>
+            </div>"`
 }
 
 function fetchGitHubInformation(event) {
@@ -28,20 +47,24 @@ function fetchGitHubInformation(event) {
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
-    $when(
-        $getJSON(`https://app.github./users/${username}`)
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response) {
-            var userData = response;
-            $("#gh-user-data").html(userInfoHTML(userData))
-        }, function(errorResponse) {
-            if (errorResponse.status=== 404 ){
-                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`)
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+            $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
-                `<h2>Error: ${errorResponse.responseJSON.massage}</h2>`)
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
-        }
-    )
+        });
 }
